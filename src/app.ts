554f84bc524093b8b08b6e6c3ff26c69bbf0c1a3
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-09-12 11:28:41 +0200

    Add CRUD operations for food categories

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import foodsRouter from "./components/foods"
+import foodsRouter from "./components/foods";
+import categoriesRouter from "./components/categories";
 const app = express();
 
 app.use(express.json());
 
 app.use("/foods", foodsRouter);
+app.use("/categories", categoriesRouter);
 
 export default app;
---
+++ src/components/categories.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { ObjectId } from "mongodb";
+const router = Router();
+
+router.get("/", async(req, res) => {
+    const catColl = req.app.locals.catColl;
+    const result = await catColl.find({}).toArray();
+    res.json(result);
+});
+
+router.get("/:id", async(req, res) => {
+    const id  = req.params.id;
+    const catColl = req.app.locals.catColl;
+    const result = await catColl.findOne({ _id: new ObjectId(id) });
+    res.json(result);
+});
+
+router.post("/", async(req, res) => {
+    const catColl = req.app.locals.catColl;
+    const body = req.body;
+    const result = await catColl.insertOne(body);
+    res.json(`Inserted a doc: id=${result.insertedId}`);
+});
+
+router.put("/:id", async(req, res) => {
+    const id = req.params.id;
+    const catColl = req.app.locals.catColl;
+    const { name } = req.body;
+    const result = await catColl.updateOne({ _id: new ObjectId(id) }, { $set: { name } });
+    res.json(result);
+})
+
+router.delete("/:id", async(req, res) => {
+    const id = req.params.id;
+    const catColl = req.app.locals.catColl;
+    const result = await catColl.deleteOne({ _id: new ObjectId(id) });
+    res.json(`Deleted ${result.deletedCount} doc(s)`);
+})
+
+export default router;
