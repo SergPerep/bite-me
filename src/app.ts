c5f2e049135155e9768b6fb858f2b6e7a099d3eb
Sergei Perepelkin <perepelkin.serge@gmail.com>
2022-09-13 11:04:56 +0200

    Set up logger and requestLogger

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import foodsRouter from "./components/foods";
 import categoriesRouter from "./components/categories";
 import brandsRouter from "./components/brands";
+import logRequests from "./utils/logRequests";
 const app = express();
 
 app.use(express.json());
+app.use(logRequests);
 
 app.use("/foods", foodsRouter);
 app.use("/categories", categoriesRouter);
